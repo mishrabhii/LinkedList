2c1bed6a6fce092e14109bbcedcbe2f1e03a347e
mishrabhii <abhi.mishra509@gmail.com>
2022-09-22 18:10:19 +0530

    Adding 30 and 56 to 70

--- LinkedList/src/com/linkedList/List.java
+++ LinkedList/src/com/linkedList/List.java
@@ -1,11 +1,46 @@
 package com.linkedList;
 
 public class List {
+	Node head;
+	Node tail;
 
 	public static void main(String[] args) {
 		System.out.println("Create a LinkedList of 56, 30, 70");
 
+		List nodeMethods = new List();
+		nodeMethods.addNode(70);
+		nodeMethods.addNode(30);
+		nodeMethods.addNode(56);
+		nodeMethods.printLinkedList();	
 	}
+
+	public void addNode(int element) {
+		Node newNode = new Node(element);
+
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		}
+		else {
+			Node temporary = head;
+			head.previous = newNode;
+			head = newNode;
+			head.next = temporary;
+		}
+	}
+
+	public void printLinkedList() {
+		Node thisNode = head;
+
+		if (thisNode == null) 
+			System.out.println("\nThe Linked list is empty.\n");
+		else {
+			System.out.println("\nDisplaying the Linked List : \n");
+			while (thisNode != null) {
+				System.out.println(thisNode.data + "\n");
+				thisNode = thisNode.next;
+			} 
+		}
+	}
+
 }
-
-
